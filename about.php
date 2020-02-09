2a67efd5e7f536a46adcdcdfd8083162767ed341
Prathaam-Modi <59971728+Prathaam-Modi@users.noreply.github.com>
2020-02-10 03:55:37 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head >
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link rel="stylesheet" href="about.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Anton&display=swap" rel="stylesheet">
+    <title>About SMIT</title>
+    <link rel="icon" href="smit.jpg" type="image/gif">
+  </head>
+
+  <body>
+<div class="container" id="container1">
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="http://localhost\D-CoderS_Hackathon_Template\Application%20Code\final_files\index.php">SMITSONIAN</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <!-- <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li> -->
+            <li><a href="http://localhost\D-CoderS_Hackathon_Template\Application%20Code\contact.html">Contact</a></li>
+              <li><a href="http://localhost\D-CoderS_Hackathon_Template\Application%20Code\gallery.html">Gallery</a></li>
+              <li><a href="http://localhost\D-CoderS_Hackathon_Template\Application%20Code\final_files\php\index.php">Login</a></li>
+              <li><a href="pirvacy\privacy.html">Privacy Policy</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+
+    <div class="jumbotron">
+       <h1>About</h1>
+     </div>
+    <div class="container">
+
+      <p id="p1"   >The first view of Sikkim Manipal Institute of Technology (SMIT) is awe inspiring and more than enough to motivate any one to be a part of this beautiful, resort like campus nestling on the banks of River Teesta in the verdant hills of East Sikkim. The institute is a constituent college of Sikkim Manipal University located in Tadong, Gangtok.  Established in 1997, SMIT is part of the renowned Manipal Group that has made its mark in the field of education and health services in India and abroad. SMIT enjoys approval by All India Council of Technical Education (AICTE) and University Grants Commission (UGC). It is also accredited by NBA. In addition, it also holds ISO 9001 accreditation vide NS-EN ISO 9001:2000.</p>
+      <h2>Infrastructure</h2>
+      <p id='p2' >
+      SMIT is one of India’s premier technology and management institutes. Spread across 35 beautifully landscaped acres, the campus is home to over 3200 students, 220 members of faculty  and a large and efficient team of support staff. The institute is reputed for its state of the art infrastructure, well stocked air-conditioned library, advanced technology laboratories, a sports complex with swimming pool, food court, separate boys and girls hostels with rooms having attached baths, 24 hours running water with hot water facilities for winters, night canteens inside hostels, a electrical sub-station for 24 hour generator support etc. . All these facilities and amenities have made SMIT one of the best and most sought after engineering and management institutes in the nation.
+      <h3>Academics</h3>
+     </p>
+     <p id='p3' >
+      The academic standard of SMIT is at par with the best institutes in the country. A highly qualified and dedicated faculty ensure that the students are provided with a holistic education that makes them job ready, industry ready and most important, life ready. The syllabus followed is prepared in consultation with industry experts and leading academicians from premier institutes like IIT Kgp, Jadavpur University, TAPMI etc. The course content in revised every three years to meet the changing demands of the industry. It is not without reason that our glittering alumni are placed with reputed organizations like Microsoft, Oracle, SAP Laps, TCS and also pursue higher studies from IIT's and IIM's.
+      Faculty : Student = 1 : 15
+     </p>
+     <h1 id=hd1>Research</h1>
+     <p id="p3">
+      Research is the life blood of a sound academic institute.  SMIT takes pride in the fact that it has emerged as a research friendly institute. A number of scholars have been awarded with PhD and many more scholars from within and outside the institute are registered for doctorate. A number of sponsored projects have also been assigned to SMIT. The faculty members are also selected to do research on about 17 sponsored projects approved and financed by DST, DBT,DRDO and AICTE. As a result, apart from books, a number of papers by the members of the faculty are also published in peer reviewed, high index journals of international repute. The students are also guided and encouraged to participate in research activities. <a href="https://smu.edu.in/smu/academics/institution-list/smit/research-smit.html">(Research@SMIT- click here)</a>
+     </p>
+     <h4>Courses</h4>
+     <p id="p4" >
+      Sikkim Manipal Institute of Technology offers a range of courses in Engineering, Basic Sciences and Management Studies. The courses offered are as follows:
+
+      BTech in Civil Engineering, Computer Science Engineering, Electrical and Electronic Engineering, Electronic and Communication Engineering, Information Technology, Mechanical Engineering, .
+      MTech in almost all branches given above
+      BBA, MBA with specialization in Marketing , Finance, Human Resources and Systems
+      BCA, MCA
+      MSc in Applied Sciences: Physics, Chemistry, Mathematics
+      Centre for Material Science and Nano Technology
+      PhD in all streams
+     </p>
+     <h5>Placements</h5>
+     <p id="p5" >
+        SMIT enjoys a strong student placement record. SMIT students have been placed in well-known national and international organizations such as, Microsoft, FL Smidth, TCS, Infosys, Wells Fargo, Cognizant, ANZ, Wipro, HCL, Tech Mahindra, Accenture, Satyam, Microland, NIIT, Tata Motors, Tractor India Ltd., Hindustan Motors, Skytech, Ushacom, Pricewaterhouse Coopers, Tata Elxsi, Hutch, ANZ, IDBI,HDFC (Mutual funds), Convergys, Times of India and many other reputed and prestigious organizations. SMIT is poised to attain even greater heights in times to come. The untiring effort of each and every member of the staff and the students to achieve that greatness in life will definitely see SMIT growing and moving from one milestone to the next with glory and success.
+     </p>
+     <h6>Education Beyond Academics</h6>
+     <p id="p6">Apart from academics, the students are provided with ample opportunities to participate in co-curricular activities so that their latent talents are nourished and allowed to flourish. Each department holds its annual fest and the much awaited annual fest of the institute, KAALRAV is held in February. These activities also help them to cultivate a spirit of healthy competitiveness.
+     </p></div></div>
+
+
+
+<!-- Site footer -->
+<footer class="site-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-6">
+          <h6>About</h6>
+
+          <p class="text">We  are  SMITSONIAN  from  SIKKIM  MANIPAL  INSTITUTE  OF  TECHNOLOGY (SMIT).We keep you updated with all the latest news about topics ranging from events to academics and everything in between.
+Read one and explore more about our glorious institution.
+</p>
+      </div>
+
+        <div class="col-xs-6 col-md-3">
+          <!-- <h6>Categories</h6>
+          <ul class="footer-links">
+            <li><a href="#">C</a></li>
+            <li><a href="#">UI Design</a></li>
+            <li><a href="#">PHP</a></li>
+            <li><a href="#">Java</a></li>
+            <li><a href="#">Android</a></li>
+            <li><a href="#">Templates</a></li>
+          </ul> -->
+        </div>
+
+        <div class="col-xs-6 col-md-3">
+          <h6>Quick Links</h6>
+          <ul class="footer-links">
+            <li><a href="http://localhost\D-CoderS_Hackathon_Template\Application%20Code\about.html">About Us</a></li>
+            <li><a href="http://localhost\D-CoderS_Hackathon_Template\Application%20Code\contact.html">Contact Us</a></li>
+
+            <li><a href="#">Privacy Policy</a></li>
+
+          </ul>
+        </div>
+      </div>
+      <hr>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-md-8 col-sm-6 col-xs-12">
+          <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by
+       <a href="#">SMITSONIAN</a>.
+          </p>
+        </div>
+        <div class="col-lg-6 col-md-4 col-sm-6 col-xs-12">
+          <ul class="social-icons">
+            <li><a class="facebook" href="https://www.facebook.com/SMIT.SMU/?ref=br_rs"><i class="fa fa-facebook"></i></a></li>
+            <li><a class="twitter" href="https://twitter.com/smitmanipal?lang=en"><i class="fa fa-twitter"></i></a></li>
+            <li><a class="youtube" href="https://www.youtube.com/channel/UCJt2l2_s6hfKE6tzEPPbuCw"><i class="fa fa-youtube"></i></a></li>
+            <li><a class="linkedin" href="https://www.linkedin.com/school/sikkim-manipal-institute-of-technology-smit-/"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <!-- </div> -->
+</footer>
+
+  </body>
+</html>
