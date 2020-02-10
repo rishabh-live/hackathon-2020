8a24beb9c7987f745ecaec3011d759d3dc8d5d35
BhramL0k <siddhantkr07@gmail.com>
2020-02-10 05:34:54 +0530

    1.10

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ error_reporting(0);
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="http://localhost/Hackathon/Homepage/index.php">SMITSONIAN</a>
+        <a class="navbar-brand" href="http://localhost/hackathon-2020/index.php">SMITSONIAN</a>
       </div>
 
       <!-- Collect the nav links, forms, and other content for toggling -->
